97a88a29b543298e15ac48ab4d448592be477a56
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-10 13:17:19 +0900

    Add Go::Schedule::Process.

---
+++ lib/SPVM/Go/Schedule/Process.pm
@@ -0,0 +1,37 @@
+package SPVM::Go::Schedule::Process;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Schedule::Process - Short Description
+
+=head1 Description
+
+Go::Schedule::Process interface of L<SPVM> has interface methods for someting.
+
+=head1 Usage
+
+  interface Go::Schedule::Process;
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
