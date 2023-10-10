363b6cee06008de0bbc8dd56500099a0483df3dd
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-10 13:51:23 +0900

    Add Go::Channel::Wait.

---
+++ lib/SPVM/Go/Channel/Wait/Queue.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Channel::Wait::Queue;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Channel::Wait::Queue - Short Description
+
+=head1 Description
+
+Go::Channel::Wait::Queue class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Channel::Wait::Queue;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
