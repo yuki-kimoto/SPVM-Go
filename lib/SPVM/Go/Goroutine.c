a4466e4a12ea9597f0598deb84828d44656c0a19
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-11 09:42:16 +0900

    Call coro_create.

--- lib/SPVM/Go/Goroutine.c
+++ lib/SPVM/Go/Goroutine.c
@@ -7,9 +7,19 @@
 
 static const char* FILE_NAME = "Go/Goroutine.c";
 
-int32_t SPVM__Go__Goroutine__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Go__Goroutine__test(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  /*
+    void coro_create (coro_context *ctx,
+                      coro_func coro,
+                      void *arg,
+                      void *sptr,
+                      size_t ssze);
+  */
+  
+  coro_context* coro_ctx = env->new_memory_block(env, stack, sizeof(coro_context));
+  
+  coro_create(coro_ctx, 0, 0, 0, 0);
   
   return 0;
 }
-
-
