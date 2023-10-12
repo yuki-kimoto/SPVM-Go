54efd3296d457587f662228b07b0e6774cbec32f
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-12 09:36:19 +0900

    Fix coro_create.

--- lib/SPVM/Go/Goroutine.c
+++ lib/SPVM/Go/Goroutine.c
@@ -7,15 +7,17 @@
 
 static const char* FILE_NAME = "Go/Goroutine.c";
 
-static void goroutine_handler (void* arg) {
-  
-  void** args = (void**)arg;
-  
-  SPVM_ENV* env = args[0];
-  SPVM_VALUE* goroutine_stack = args[1];
-  void* obj_callback = args[2];
+static void goroutine_handler (void* obj_goroutine_argument) {
   
   int32_t error_id = 0;
+  
+  void** goroutine_argument = (void**)SPVM_NATIVE_GET_POINTER(obj_goroutine_argument);
+  
+  SPVM_ENV* env = goroutine_argument[0];
+  
+  SPVM_VALUE* goroutine_stack = goroutine_argument[1];
+  
+  void* obj_callback = goroutine_argument[2];
   
   {
     env->call_instance_method_by_name(env, goroutine_stack, "", 0, &error_id, __func__, FILE_NAME, __LINE__);
@@ -36,11 +38,21 @@ int32_t SPVM__Go__Goroutine__init_goroutine(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   void* obj_self = stack[0].oval;
   
-  void* obj_callback = stack[1].oval;
+  void* obj_goroutine_argument = stack[1].oval;
   
-  if (!obj_callback) {
-    return env->die(env, stack, "$callback must be defined.", __func__, FILE_NAME, __LINE__);
+  coro_context* goroutine = env->new_memory_block(env, stack, sizeof(coro_context));
+  
+  struct coro_stack* coro_goroutine_stack = env->new_memory_block(env, stack, sizeof(struct coro_stack));
+  
+  int32_t stack_size;
+  if (sizeof(SPVM_VALUE) > sizeof(void*)) {
+    stack_size = 512 * sizeof(void*) * 2;
   }
+  else {
+    stack_size = 512 * sizeof(void*);
+  }
+  
+  coro_stack_alloc(coro_goroutine_stack, stack_size);
   
   /*
     void coro_create (coro_context *ctx,
@@ -50,21 +62,11 @@ int32_t SPVM__Go__Goroutine__init_goroutine(SPVM_ENV* env, SPVM_VALUE* stack) {
                       size_t ssze);
   */
   
-  coro_context* goroutine = env->new_memory_block(env, stack, sizeof(coro_context));
+  coro_create(goroutine, goroutine_handler, obj_goroutine_argument, coro_goroutine_stack->sptr, coro_goroutine_stack->ssze);
   
-  int32_t stack_size = sizeof(SPVM_VALUE) * 512;
-  
-  SPVM_VALUE* goroutine_stack = env->new_stack(env);
-  
-  struct coro_stack* coro_goroutine_stack = env->new_memory_block(env, stack, sizeof(struct coro_stack));
-  
-  coro_stack_alloc(coro_goroutine_stack, stack_size);
-  
-  coro_create(goroutine, goroutine_handler, obj_self, coro_goroutine_stack->sptr, coro_goroutine_stack->ssze);
+  env->set_pointer(env, stack, obj_self, goroutine);
   
   coro_destroy(goroutine);
-  
-  env->free_stack(env, goroutine_stack);
   
   return 0;
 }
