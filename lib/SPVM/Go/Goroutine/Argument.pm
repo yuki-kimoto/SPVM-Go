3463448d6b7a08317336f97e70445a8dd67674bf
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-12 09:29:01 +0900

    Add Go::Goroutine::Argument.

---
+++ lib/SPVM/Go/Goroutine/Argument.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Goroutine::Argument;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Goroutine::Argument - Short Description
+
+=head1 Description
+
+Go::Goroutine::Argument class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Goroutine::Argument;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
