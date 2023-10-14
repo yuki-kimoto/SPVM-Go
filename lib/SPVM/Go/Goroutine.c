54afd976a3c871a57180317bdebc50dd83a47a15
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-15 08:39:47 +0900

    Add tests of goroutine, but has memory error.

--- lib/SPVM/Go/Goroutine.c
+++ lib/SPVM/Go/Goroutine.c
@@ -9,6 +9,8 @@ static const char* FILE_NAME = "Go/Goroutine.c";
 
 static void goroutine_handler (void* obj_self) {
   
+  warn("LINE %d", __LINE__);
+  
   int32_t error_id = 0;
   
   void** pointer_items = (void**)SPVM_NATIVE_GET_POINTER(obj_self);
@@ -23,18 +25,29 @@ static void goroutine_handler (void* obj_self) {
     
     const char* exception = env->get_chars(env, goroutine_stack, obj_exception);
     
+    warn("LINE %d", __LINE__);
+    
     spvm_warn("%s", exception);
   }
   
+  warn("LINE %d", __LINE__);
+  
   goroutine_stack[0].oval = obj_callback;
   env->call_instance_method_by_name(env, goroutine_stack, "", 0, &error_id, __func__, FILE_NAME, __LINE__);
+  
+  warn("LINE %d", __LINE__);
   
   if (error_id) {
     void* obj_exception = env->get_exception(env, goroutine_stack);
     
     const char* exception = env->get_chars(env, goroutine_stack, obj_exception);
     
+    warn("LINE %d", __LINE__);
+    
     spvm_warn("%s", exception);
+    
+    warn("LINE %d", __LINE__);
+    
   }
   
   return;
@@ -76,20 +89,35 @@ int32_t SPVM__Go__Goroutine__transfer(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   void* obj_goroutine_from = stack[0].oval;
   
+  if (!obj_goroutine_from) {
+    return env->die(env, stack, "$from must be defined.", __func__, FILE_NAME, __LINE__);
+  }
+  
   void** goroutine_from_pointer_items = env->get_pointer(env, stack, obj_goroutine_from);
   
   coro_context* goroutine_from_goroutine_context = goroutine_from_pointer_items[0];
   
   void* obj_goroutine_to = stack[1].oval;
   
+  if (!obj_goroutine_to) {
+    return env->die(env, stack, "$to must be defined.", __func__, FILE_NAME, __LINE__);
+  }
+  
   void** goroutine_to_pointer_items = env->get_pointer(env, stack, obj_goroutine_to);
   
   coro_context* goroutine_to_goroutine_context = goroutine_to_pointer_items[0];
   
+  warn("transfer LINE %d", __LINE__);
+  
   coro_transfer(goroutine_from_goroutine_context, goroutine_to_goroutine_context);
+  
+  warn("transfer LINE %d", __LINE__);
+  
 }
 
 int32_t SPVM__Go__Goroutine__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  warn("DESTROY %d", __LINE__);
   
   void* obj_self = stack[0].oval;
   
