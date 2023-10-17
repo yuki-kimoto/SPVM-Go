f978c63b2c8240685ed3e813ec7869e27c9496f4
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-17 13:33:39 +0900

    Add Go::Schedule::Monitor.

---
+++ lib/SPVM/Go/Schedule/Monitor.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Schedule::Monitor;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Schedule::Monitor - Short Description
+
+=head1 Description
+
+Go::Schedule::Monitor class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Schedule::Monitor;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
