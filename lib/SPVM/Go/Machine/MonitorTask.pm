1d3b0336e1706bdce5682012f95856c7f81ad7c1
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-18 09:38:46 +0900

    Add Go::Machine::Monitor, Go::Machine::ProcessTask.

---
+++ lib/SPVM/Go/Machine/MonitorTask.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Machine::MonitorTask;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Machine::MonitorTask - Short Description
+
+=head1 Description
+
+Go::Machine::MonitorTask class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Machine::MonitorTask;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
---
+++ lib/SPVM/Go/Machine/ProcessTask.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Machine::ProcessTask;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Machine::ProcessTask - Short Description
+
+=head1 Description
+
+Go::Machine::ProcessTask class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Machine::ProcessTask;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
