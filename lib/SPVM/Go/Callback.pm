be2409af7e7749812b6f39017a38f62523d96c24
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-18 13:02:49 +0900

    Add Go::Callback.

---
+++ lib/SPVM/Go/Callback.pm
@@ -0,0 +1,37 @@
+package SPVM::Go::Callback;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Callback - Short Description
+
+=head1 Description
+
+Go::Callback interface of L<SPVM> has interface methods for someting.
+
+=head1 Usage
+
+  interface Go::Callback;
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
