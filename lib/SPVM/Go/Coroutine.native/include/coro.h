d76b62426709f0b28da744a283b9d8c007d2f31e
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-03 10:16:33 +0900

    Fix mac bug.

--- lib/SPVM/Go/Coroutine.native/include/coro.h
+++ lib/SPVM/Go/Coroutine.native/include/coro.h
@@ -384,10 +384,10 @@ struct coro_context
   coro_jmp_buf env;
 };
 
-# define coro_transfer(p,n) do { if (!coro_setjmp ((p)->env)) coro_longjmp ((n)->env); } while (0)
+# define coro_transfer(p,n) do { if (!coro_setjmp (((struct coro_context*)p)->env)) coro_longjmp (((struct coro_context*)n)->env); } while (0)
 # define coro_destroy(ctx) (void *)(ctx)
 
-#elif CORO_ASM
+#elif defined CORO_ASM
 
 struct coro_context
 {
