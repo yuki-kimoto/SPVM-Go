187b79a9dc34c5f386523ddd400d9c6dcf1bbd9c
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-07 09:05:56 +0900

    Add Go::Select doc.

--- lib/SPVM/Go/Select.pm
+++ lib/SPVM/Go/Select.pm
@@ -6,11 +6,11 @@ package SPVM::Go::Select;
 
 =head1 Name
 
-SPVM::Go::Select - Short Description
+SPVM::Go::Select - Go select Statement
 
 =head1 Description
 
-Go::Select class of L<SPVM> has methods for someting.
+Go::Select class of L<SPVM> has methods to select a readable/writable channel.
 
 =head1 Usage
 
@@ -18,24 +18,55 @@ Go::Select class of L<SPVM> has methods for someting.
 
 =head1 Fields
 
+=head2 non_blocking
 
+C<has non_blocking : rw byte;>
 
-=head1 Class Methods
-
-
+Set the flag if this select is non-blocking.
 
 =head1 Instance Methods
 
+=head2 add_read
 
+C<method add_read : void ($channel : L<Go::Channel|SPVM::Go::Channe>);>
 
+Adds a select case for reading given a channel $channel.
 
-=head1 Repository
+=head2 add_write
 
+C<method add_write : void ($channel : L<Go::Channel|SPVM::Go::Channe>, $value : object);>
 
+Adds a select case for writing given a channel $channel and a value $value.
 
-=head1 Author
+=head2 remove_read
 
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+C<method remove_read : void ($channel : L<Go::Channel|SPVM::Go::Channe>);>
+
+Removes a corresponding select case for reading given a channel $channel.
+
+=head2 remove_write
+
+C<method remove_write : void ($channel : L<Go::Channel|SPVM::Go::Channe>);>
+
+Removes a corresponding select case for reading given a channel $channel.
+
+=head2 select
+
+C<method select : L<Go::Select::Result|SPVM::Go::Select::Result> ();>
+
+Selects a channel and returns its result.
+
+A readable or writable channel is randomly selected.
+
+If no channels are readable or writable, the goroutine waits until a readable or writable channel is selected.
+
+If the L</"non_blocking"> field is set to non-zero and no channels are readable or writable, returns undef.
+
+=head2 is_empty
+
+C<method is_empty : int ();>
+
+If the count of the select cases is 0, returns 1. Otherwise returns 0.
 
 =head1 Copyright & License
 
