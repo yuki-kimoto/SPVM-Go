14935fa0c4e9c51084a91ef90d73d8369c73b44b
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-01-12 13:43:08 +0900

    Add Go::Context::Derived, CancelCallback.

---
+++ lib/SPVM/Go/Context/CancelCallback.pm
@@ -0,0 +1,43 @@
+package SPVM::Go::Context::CanncelCallback;
+
+
+
+1;
+
+=encoding utf8
+
+=head1 Name
+
+SPVM::Go::Context::CanncelCallback - Short Description
+
+=head1 Description
+
+Go::Context::CanncelCallback interface in L<SPVM> has interface methods to do someting.
+
+=head1 Usage
+
+  interface Go::Context::CanncelCallback;
+
+=head1 Details
+
+
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2026 Yuki Kimoto
+
+MIT License
+
