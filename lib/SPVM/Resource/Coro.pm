cb9f7eabc6aaa15a0f27b5d78541564c35047b2a
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-01 15:22:14 +0900

    Add doc for Resource::Coro.

--- lib/SPVM/Resource/Coro.pm
+++ lib/SPVM/Resource/Coro.pm
@@ -8,11 +8,11 @@ package SPVM::Resource::Coro;
 
 =head1 Name
 
-SPVM::Resource::Coro - Short Description
+SPVM::Resource::Coro - libcoro Resources
 
 =head1 Description
 
-Resource::Coro class in L<SPVM> is a L<resource|SPVM::Document::Resource> class for something.
+Resource::Coro class in L<SPVM> is a L<resource|SPVM::Document::Resource> class for libcoro.
 
 =head1 Usage
 
@@ -27,83 +27,91 @@ MyClass.config:
 MyClass.c:
 
   #include "spvm_native.h"
-  #include "foo.h"
-  
-  
+  #include "coro.h"
   
   int32_t SPVM__MyClass__test(SPVM_ENV* env, SPVM_VALUE* stack) {
     
-    // Use functions in foo.h
+    // Context struct
+    coro_context ctx;
+
+    // Initialize context
+    coro_create(&ctx, coro_func, 0, stack, stack_size);
     
     return 0;
   }
   
-  
-  
-=head1 Details
-
-
-
 =head1 Original Product
 
-
+L<libcoro|http://software.schmorp.de/pkg/libcoro.html>
 
 =head1 Original Product Version
 
-
+Latest version (No versioning scheme). Use the code bundled in L<Coro 6.57|https://metacpan.org/release/MLEHMANN/Coro-6.57> on CPAN.
 
 =head1 Language
 
-
+C language
 
 =head1 Language Standard
 
-
-
-=head1 Required Libraries
-
-
-
-=head1 Required Linker Flags
-
-
-
-=head1 Required Resources
-
-
+C99
 
 =head1 Header Files
 
+=over 2
 
+=item * C<coro.h>
+
+=back
 
 =head1 Source Files
 
+=over 2
 
+=item * C<coro.c>
+
+=back
 
 =head1 Compiler Flags
 
+The compiler flags are automatically configured based on the OS and its environment to ensure the correct context switching mechanism (e.g., C, C<setjmp/longjmp>, or C).
 
+Typical flags include:
+
+=over 2
+
+=item * C<-DCORO_UCONTEXT> (used when C<ucontext.h> is available)
+
+=item * C<-DCORO_SJLJ> (used for C<setjmp/longjmp> based switching)
+
+=item * C<-DCORO_ASM> (used for assembly-based switching)
+
+=item * C<-DCORO_PTHREAD> (used when C are required)
+
+=item * C<-D_FORTIFY_SOURCE=0> (often required for C<setjmp/longjmp> stability)
+
+=back
+
+The actual flags applied are determined at build-time by detecting the target platform and specific system requirements (such as thread support and stack alignment).
 
 =head1 How to Create Resource
 
-
-
 =head2 Donwload
 
-
+  mkdir -p .tmp
+  curl -L https://cpan.metacpan.org/authors/id/M/ML/MLEHMANN/Coro-6.57.tar.gz | tar -xz -C .tmp
 
 =head2 Extracting Source Files
 
-
+  cp .tmp/Coro-6.57/Coro/libcoro/coro.h lib/SPVM/Resource/Coro.native/include/
 
 =head2 Extracting Header Files
 
-
-
+  cp .tmp/Coro-6.57/Coro/libcoro/coro.c lib/SPVM/Resource/Coro.native/src/
 
 =head1 Repository
 
-
+L<SPVM::Resource::Coro - Github|https://github.com/yuki-kimoto/SPVM-Resource-Coro>
 
 =head1 Author
 
