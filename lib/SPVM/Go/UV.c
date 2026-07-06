0dbcdad202d131628ffd0ea111863f1845625e41
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-06 10:52:17 +0900

    Copy Go::Goroutine libuv sources to Go::UV.

---
+++ lib/SPVM/Go/UV.c
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 Yuki Kimoto
+// MIT License
+
+#include <assert.h>
+
+#include "spvm_native.h"
+#include "uv.h"
+
+static const char* FILE_NAME = "Go/UV.c";
+
+static void uv_test () {
+  uv_tcp_t handle;
+}
+
