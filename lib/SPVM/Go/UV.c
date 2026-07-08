a1401b5e96523f970fe564d6342ef849e49526f7
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-08 09:55:27 +0900

    Implement SPVM__Go__UV__run_default, SPVM__Go__UV__run_nowait.

--- lib/SPVM/Go/UV.c
+++ lib/SPVM/Go/UV.c
@@ -12,16 +12,30 @@ void boot_Go__UV(void) {
   
 }
 
-static void uv_test () {
-  uv_tcp_t handle;
-}
+static uv_loop_t* uv_loop = NULL;
 
 int32_t SPVM__Go__UV__run_default(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  uv_loop_t* uv_loop = uv_default_loop();
+  
+  int32_t status = uv_run(uv_loop, UV_RUN_DEFAULT);
+  
+  if (!(status == 0)) {
+    return env->die(env, stack, "[uv Error]uv_run failed. $mode=%d", __func__, FILE_NAME, __LINE__, UV_RUN_DEFAULT);
+  }
   
   return 0;
 }
 
 int32_t SPVM__Go__UV__run_nowait(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  uv_loop_t* uv_loop = uv_default_loop();
+  
+  int32_t status = uv_run(uv_loop, UV_RUN_NOWAIT);
+  
+  if (!(status == 0)) {
+    return env->die(env, stack, "[uv Error]uv_run failed. $mode=%d", __func__, FILE_NAME, __LINE__, UV_RUN_NOWAIT);
+  }
   
   return 0;
 }
