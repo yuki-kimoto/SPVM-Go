bdabfd2624a4e0d02b4af05072a6ff63d3901118
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-09 14:47:40 +0900

    Remove run_default, run_nowait.

--- lib/SPVM/Go/UV.c
+++ lib/SPVM/Go/UV.c
@@ -12,24 +12,11 @@ void boot_Go__UV(void) {
   
 }
 
-int32_t SPVM__Go__UV__run_default(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Go__UV__run(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   uv_loop_t* uv_loop = uv_default_loop();
   
   int32_t status = uv_run(uv_loop, UV_RUN_DEFAULT);
-  
-  stack[0].ival = status;
-  
-  return 0;
-}
-
-int32_t SPVM__Go__UV__run_nowait(SPVM_ENV* env, SPVM_VALUE* stack) {
-  
-  uv_loop_t* uv_loop = uv_default_loop();
-  
-  int32_t status = uv_run(uv_loop, UV_RUN_NOWAIT);
-  
-  stack[0].ival = status;
   
   return 0;
 }
