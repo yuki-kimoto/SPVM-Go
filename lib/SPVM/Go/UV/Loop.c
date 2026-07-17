c42ff49858f00dcc733df286decb2de376b06514
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-17 10:44:11 +0900

    Use defaut loop for Windows temporary.

--- lib/SPVM/Go/UV/Loop.c
+++ lib/SPVM/Go/UV/Loop.c
@@ -10,12 +10,9 @@ int32_t SPVM__Go__UV__Loop__new(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t error_id = 0;
   
-  // Force libuv global initialization
-  uv_default_loop();
+  uv_loop_t* uv_loop = uv_default_loop();
   
-  uv_loop_t* uv_loop = env->new_memory_block(env, stack, sizeof(uv_loop_t));
-  
-  uv_loop_init(uv_loop);
+  // uv_loop_init(uv_loop);
   
   SPVM_OBJ* obj_self = env->new_pointer_object_by_name(env, stack, "Go::UV::Loop", uv_loop, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) return error_id;
@@ -32,9 +29,9 @@ int32_t SPVM__Go__UV__Loop__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
   uv_loop_t* uv_loop = env->get_pointer(env, stack, obj_self);
   assert(uv_loop);
   
-  uv_loop_close(uv_loop);
+  // uv_loop_close(uv_loop);
   
-  env->free_memory_block(env, stack, uv_loop);
+  // env->free_memory_block(env, stack, uv_loop);
   
   return 0;
 }
