95b6bc2c7f3be2fed3010460b2efe6db94604845
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-21 12:17:45 +0900

    Mrge async_init to async_new.

--- lib/SPVM/Go/UV/Loop.c
+++ lib/SPVM/Go/UV/Loop.c
@@ -375,30 +375,13 @@ int32_t SPVM__Go__UV__Loop__handle_async_new(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   handle_data->obj_uv_handle = obj_uv_handle;
   
-  stack[0].oval = obj_uv_handle;
-  
-  return 0;
-}
-
-int32_t SPVM__Go__UV__Loop__async_init(SPVM_ENV* env, SPVM_VALUE* stack) {
-  
-  int32_t error_id = 0;
-  
-  SPVM_OBJ* obj_uv_loop = stack[0].oval;
-  
-  SPVM_OBJ* obj_uv_handle = stack[1].oval;
-  
-  assert(obj_uv_loop);
-  
-  assert(obj_uv_handle);
-  
-  uv_loop_t* uv_loop = env->get_pointer(env, stack, obj_uv_loop);
-  uv_async_t* uv_handle = env->get_pointer(env, stack, obj_uv_handle);
   int32_t status = uv_async_init(uv_loop, uv_handle, SPVM__Go__UV__Loop__cb);
   
   if (!(status == 0)) {
     return env->die(env, stack, "uv_async_init failed. status=%d.", __func__, FILE_NAME, __LINE__, status);
   }
+  
+  stack[0].oval = obj_uv_handle;
   
   return 0;
 }
