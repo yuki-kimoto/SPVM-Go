cf1fa71f37e7cdbcbb8127a7d54e3f9fe2f03fd5
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-22 13:35:59 +0900

    Use new_async.

--- lib/SPVM/Go/UV/Loop.c
+++ lib/SPVM/Go/UV/Loop.c
@@ -334,11 +334,11 @@ int32_t SPVM__Go__UV__Loop__handle_async_send_v2(SPVM_ENV* env, SPVM_VALUE* stac
   int32_t error_id = 0;
   
   SPVM_OBJ* obj_uv_loop = stack[0].oval;
-  SPVM_OBJ* obj_uv_handle = stack[1].oval;
+  SPVM_OBJ* obj_uv_async = stack[1].oval;
   
-  uv_async_t* uv_handle = (uv_async_t*)env->get_pointer(env, stack, obj_uv_handle);
+  uv_async_t* uv_async = (uv_async_t*)env->get_pointer(env, stack, obj_uv_async);
   
-  uv_async_send(uv_handle);
+  uv_async_send(uv_async);
   
   return 0;
 }
