6625eada5f6e941326463843e0bd139e14f23b18
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-23 13:36:50 +0900

    Change variable names.

--- lib/SPVM/Go/UV/Loop.c
+++ lib/SPVM/Go/UV/Loop.c
@@ -13,7 +13,7 @@ typedef struct {
   uv_handle_t* related_handle;
 } SPVM__Go__UV__Loop__HANDLE_DATA;
 
-static void SPVM__Go__UV__Loop__close_cb_v2(uv_handle_t* handle) {
+static void SPVM__Go__UV__Loop__close_cb(uv_handle_t* handle) {
   
   int32_t error_id = 0;
   
@@ -444,7 +444,7 @@ int32_t SPVM__Go__UV__Loop__handle_close(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   uv_handle_t* uv_handle = env->get_pointer(env, stack, obj_uv_handle);
   
-  uv_close(uv_handle, SPVM__Go__UV__Loop__close_cb_v2);
+  uv_close(uv_handle, SPVM__Go__UV__Loop__close_cb);
   
   return 0;
 }
