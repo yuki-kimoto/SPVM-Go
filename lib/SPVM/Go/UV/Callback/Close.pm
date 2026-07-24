b54741c9f56e47699c7b3e3a83b4a7fb184d3762
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-24 15:11:56 +0900

    Add Go::UV::Callback::Close.

---
+++ lib/SPVM/Go/UV/Callback/Close.pm
@@ -0,0 +1,71 @@
+package SPVM::Go::UV::Callback::Close;
+
+
+
+1;
+
+=encoding utf8
+
+=head1 Name
+
+SPVM::Go::UV::Callback::Close - Short Description
+
+=head1 Description
+
+Go::UV::Callback::Close class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Go::UV::Callback::Close;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Well Known Child Classes
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2026 Yuki Kimoto
+
+MIT License
+
