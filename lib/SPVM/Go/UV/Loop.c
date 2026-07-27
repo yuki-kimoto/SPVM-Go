f93baba952bf3ab309f813a861c9577a4b802990
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-27 14:05:55 +0900

    Add Go::UV#strerror.

--- lib/SPVM/Go/UV/Loop.c
+++ lib/SPVM/Go/UV/Loop.c
@@ -26,6 +26,22 @@ int32_t SPVM__Go__UV__Loop__default_loop(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
+int32_t SPVM__Go__UV__Loop__strerror(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  int32_t status = stack[0].ival;
+  
+  char uv_strerror[512];
+  uv_strerror_r(status, uv_strerror, sizeof(uv_strerror));
+  
+  SPVM_OBJ* obj_uv_strerror = env->new_string_nolen(env, stack, uv_strerror);
+  
+  stack[0].oval = obj_uv_strerror;
+  
+  return 0;
+}
+
 static void SPVM__Go__UV__Loop__close_cb(uv_handle_t* uv_handle) {
   
   int32_t error_id = 0;
