a12a810ddb11830afdec5b35a2ded206a837835e
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-30 13:58:58 +0900

    Add doc for Go::UV::Callback::Write.

--- lib/SPVM/Go/UV/Callback/Write.pm
+++ lib/SPVM/Go/UV/Callback/Write.pm
@@ -8,64 +8,28 @@ package SPVM::Go::UV::Callback::Write;
 
 =head1 Name
 
-SPVM::Go::UV::Callback::Write - Short Description
+SPVM::Go::UV::Callback::Write - Callback for Write Request
 
 =head1 Description
 
-Go::UV::Callback::Write class in L<SPVM> has methods to do someting.
+C<Go::UV::Callback::Write> in L<SPVM> is the interface for the callback passed to L<uv_write|https://docs.libuv.org/en/v1.x/stream.html#c.uv_write>.
 
 =head1 Usage
 
   use Go::UV::Callback::Write;
 
-=head1 Details
+=head1 Interface Methods
 
+=head2 Anonymous method
 
+C<required method : void ($uv_req_write : L<Go::UV::Request::Write|SPVM::Go::UV::Request::Write>, $status : int);>
 
-=head1 Super Class
+The callback method called when the write request has completed.
 
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
-=head1 Instance Methods
-
-
-
-=head1 Well Known Child Classes
-
-
-
-=head1 See Also
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+See L<Go::UV::Request::Write#write|SPVM::Go::UV::Request::Write/"write"> method for usage.
 
 =head1 Copyright & License
 
 Copyright (c) 2026 Yuki Kimoto
 
 MIT License
-
