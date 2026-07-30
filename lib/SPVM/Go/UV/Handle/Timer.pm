b890338a8f7eda1ffdc0aa236b2cadd92430ed4a
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-30 14:35:13 +0900

    Add doc for Go::UV::Handle::Timer.

--- lib/SPVM/Go/UV/Handle/Timer.pm
+++ lib/SPVM/Go/UV/Handle/Timer.pm
@@ -8,64 +8,52 @@ package SPVM::Go::UV::Handle::Timer;
 
 =head1 Name
 
-SPVM::Go::UV::Handle::Timer - Short Description
+SPVM::Go::UV::Handle::Timer - Timer Handle for libuv
 
 =head1 Description
 
-Go::UV::Handle::Timer class in L<SPVM> has methods to do someting.
+C<Go::UV::Handle::Timer> in L<SPVM> represents the timer handle for L<libuv|https://libuv.org/>, corresponding to the C<uv_timer_t> structure.
+
+=head1 Super Class
+
+L<Go::UV::Handle|SPVM::Go::UV::Handle>
 
 =head1 Usage
 
   use Go::UV::Handle::Timer;
-
-=head1 Details
-
-
-
-=head1 Super Class
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
+  
+  my $timer = Go::UV::Handle::Timer->new;
 
 =head1 Fields
 
+=head2 timer_cb
 
+C<has timer_cb : rw L<Go::UV::Callback::Timer|SPVM::Go::UV::Callback::Timer>;>
+
+The callback called when the timer timeout expires.
 
 =head1 Class Methods
 
+=head2 new
 
+C<static method new : L<Go::UV::Handle::Timer|SPVM::Go::UV::Handle::Timer> ();>
+
+Creates a new L<Go::UV::Handle::Timer|SPVM::Go::UV::Handle::Timer> object, and returns it.
 
 =head1 Instance Methods
 
+=head2 start
 
+C<method start : void ($cb : L<Go::UV::Callback::Timer|SPVM::Go::UV::Callback::Timer>, $timeout_msec : long, $interval_msec : long = 0);>
 
-=head1 Well Known Child Classes
+Starts the timer with a timeout of C<$timeout_msec> milliseconds and a repeat interval of C<$interval_msec> milliseconds.
 
+The callback C<$cb> is stored in L</"timer_cb"> field.
 
-
-=head1 See Also
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+The callback C<$cb> is invoked when the timer timeout expires.
 
 =head1 Copyright & License
 
 Copyright (c) 2026 Yuki Kimoto
 
 MIT License
-
