a60509bd77fafc17915013653b1fceb16e28ffd0
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-30 10:05:23 +0900

    Add document for Go::UV.

--- lib/SPVM/Go/UV.pm
+++ lib/SPVM/Go/UV.pm
@@ -8,56 +8,103 @@ package SPVM::Go::UV;
 
 =head1 Name
 
-SPVM::Go::UV - Short Description
+SPVM::Go::UV - libuv Binding
 
 =head1 Description
 
-Go::UV class in L<SPVM> has methods to do someting.
+Go::UV class in L<SPVM> is a L<libuv|https://libuv.org/> binding.
 
 =head1 Usage
 
-  use Go::UV;
+  use Go::UV::Loop;
+  use Go::UV::Constant as UV_C;
+  
+  my $uv_loop = Go::UV::Loop->new;
+  
+  # Do someting
+  
+  $uv_loop->run(UV_C->UV_RUN_DEFAULT);
+  
+Timer:
 
-=head1 Details
+  use Go::UV::Handle::Timer;
+  
+  my $timeout_msec = 3_000;
+  my $uv_timer = Go::UV::Handle::Timer->new;
+  $uv_loop->timer_init($uv_timer);
+  my $timer_cb = method : void ($uv_timer : Go::UV::Handle::Timer) {
+    
+    # Do something
+    
+    $uv_timer->close;
+  };
+  $uv_timer->start($timer_cb, $timeout_msec);
 
+Socket IO:
 
-
-=head1 Super Class
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
+  use Go::UV::Handle::Poll;
+  
+  my $uv_poll = Go::UV::Handle::Poll->new;
+  $uv_loop->poll_init($uv_poll, $fd);
+  my $poll_cb = method : void ($uv_poll : Go::UV::Handle::Poll, $status : int, $events : int) {
+    
+    # Do something
+    
+    $poll_cb->close;
+  };
+  
+  my $events = UV_C->UV_READABLE | UV_C->UV_WRITABLE;
+  $uv_poll->start($poll_cb, $events);
 
 =head1 Class Methods
 
+C<static method strerror : string ($status : int);>
 
+Gets human-readable description for an error code by calling L<uv_strerror_r|https://docs.libuv.org/en/v1.x/errors.html#c.uv_strerror_r>, and returns it as a string.
 
-=head1 Instance Methods
+=head1 Modules
 
+=over 2
 
+=item * L<Go::UV::Loop|SPVM::Go::UV::Loop>
 
-=head1 Well Known Child Classes
+=item * L<Go::UV::Constant|SPVM::Go::UV::Constant>
 
+=item * L<Go::UV::Handle|SPVM::Go::UV::Handle>
 
+=item * L<Go::UV::Handle::Idle|SPVM::Go::UV::Handle::Idle>
 
-=head1 See Also
+=item * L<Go::UV::Handle::Pipe|SPVM::Go::UV::Handle::Pipe>
 
+=item * L<Go::UV::Handle::Async|SPVM::Go::UV::Handle::Async>
 
+=item * L<Go::UV::Handle::Stream|SPVM::Go::UV::Handle::Stream>
 
+=item * L<Go::UV::Handle::Timer|SPVM::Go::UV::Handle::Timer>
+
+=item * L<Go::UV::Handle::Poll|SPVM::Go::UV::Handle::Poll>
+
+=item * L<Go::UV::Request|SPVM::Go::UV::Request>
+
+=item * L<Go::UV::Request::Write|SPVM::Go::UV::Request::Write>
+
+=item * L<Go::UV::Callback::Write|SPVM::Go::UV::Callback::Write>
+
+=item * L<Go::UV::Callback::Idle|SPVM::Go::UV::Callback::Idle>
+
+=item * L<Go::UV::Callback::Async|SPVM::Go::UV::Callback::Async>
+
+=item * L<Go::UV::Callback::Read|SPVM::Go::UV::Callback::Read>
+
+=item * L<Go::UV::Callback::Close|SPVM::Go::UV::Callback::Close>
+
+=item * L<Go::UV::Callback::Timer|SPVM::Go::UV::Callback::Timer>
+
+=item * L<Go::UV::Callback::Poll|SPVM::Go::UV::Callback::Poll>
+
+=back
 
 =head1 Repository
-
-
 
 =head1 Author
 
