6576f6575d633edd9286476801c65b10f154b6a3
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-30 14:24:03 +0900

    Add doc for Go::UV::Handle::Idle.

--- lib/SPVM/Go/UV/Handle/Idle.pm
+++ lib/SPVM/Go/UV/Handle/Idle.pm
@@ -8,64 +8,52 @@ package SPVM::Go::UV::Handle::Idle;
 
 =head1 Name
 
-SPVM::Go::UV::Handle::Idle - Short Description
+SPVM::Go::UV::Handle::Idle - Idle Handle for libuv
 
 =head1 Description
 
-Go::UV::Handle::Idle class in L<SPVM> has methods to do someting.
+C<Go::UV::Handle::Idle> in L<SPVM> represents the idle handle for L<libuv|https://libuv.org/>, corresponding to the C<uv_idle_t> structure.
+
+=head1 Super Class
+
+L<Go::UV::Handle|SPVM::Go::UV::Handle>
 
 =head1 Usage
 
   use Go::UV::Handle::Idle;
-
-=head1 Details
-
-
-
-=head1 Super Class
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
+  
+  my $idle = Go::UV::Handle::Idle->new;
 
 =head1 Fields
 
+=head2 idle_cb
 
+C<has idle_cb : rw L<Go::UV::Callback::Idle|SPVM::Go::UV::Callback::Idle>;>
+
+The callback called when the idle handle is active.
 
 =head1 Class Methods
 
+=head2 new
 
+C<static method new : L<Go::UV::Handle::Idle|SPVM::Go::UV::Handle::Idle> ();>
+
+Creates a new L<Go::UV::Handle::Idle|SPVM::Go::UV::Handle::Idle> object, and returns it.
 
 =head1 Instance Methods
 
+=head2 start
 
+C<method start : void ($cb : L<Go::UV::Callback::Idle|SPVM::Go::UV::Callback::Idle>);>
 
-=head1 Well Known Child Classes
+Starts the idle handle with the callback C<$cb>. 
 
+The callback C<$cb> is stored in L</"idle_cb"> field.
 
-
-=head1 See Also
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+The callback C<$cb> is invoked when the idle handle is active.
 
 =head1 Copyright & License
 
 Copyright (c) 2026 Yuki Kimoto
 
 MIT License
-
