a162b321af6c045b056c69236105c20d5d56758b
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-07-30 14:14:20 +0900

    Add doc for Go::UV::Request::Write.

--- lib/SPVM/Go/UV/Request/Write.pm
+++ lib/SPVM/Go/UV/Request/Write.pm
@@ -8,64 +8,54 @@ package SPVM::Go::UV::Request::Write;
 
 =head1 Name
 
-SPVM::Go::UV::Request::Write - Short Description
+SPVM::Go::UV::Request::Write - Write Request for libuv
 
 =head1 Description
 
-Go::UV::Request::Write class in L<SPVM> has methods to do someting.
+C<Go::UV::Request::Write> in L<SPVM> represents the write request for L<libuv|https://libuv.org/>, corresponding to the C<uv_write_t> structure. It inherits from L<Go::UV::Request|SPVM::Go::UV::Request>.
 
 =head1 Usage
 
   use Go::UV::Request::Write;
-
-=head1 Details
-
-
-
-=head1 Super Class
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
+  
+  my $req_write = Go::UV::Request::Write->new;
 
 =head1 Fields
 
+=head2 write_cb
 
+C<has write_cb : rw L<Go::UV::Callback::Write|SPVM::Go::UV::Callback::Write>;>
+
+The callback called when the write request has completed.
+
+=head2 write_buffer
+
+C<has write_buffer : rw string;>
+
+The buffer associated with the write request.
 
 =head1 Class Methods
 
+=head2 new
 
+C<static method new : L<Go::UV::Request::Write|SPVM::Go::UV::Request::Write> ();>
+
+Creates a new L<Go::UV::Request::Write|SPVM::Go::UV::Request::Write> object, and returns it.
 
 =head1 Instance Methods
 
+=head2 write
 
+C<method write : void ($uv_stream : L<Go::UV::Handle::Stream|SPVM::Go::UV::Handle::Stream>, $buffer : string, $buffer_length : int, $cb : L<Go::UV::Callback::Write|SPVM::Go::UV::Callback::Write>, $buffer_offset : int = 0);>
 
-=head1 Well Known Child Classes
+Writes data from $buffer with length $buffer_length and offset $buffer_offset to the stream $uv_stream. 
 
+The callback $cb is stored in L</"write_cb"> field. The buffer $buffer is stored in L</"write_buffer"> field.
 
-
-=head1 See Also
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+The callback $cb is invoked when the write operation completes.
 
 =head1 Copyright & License
 
 Copyright (c) 2026 Yuki Kimoto
 
 MIT License
-
